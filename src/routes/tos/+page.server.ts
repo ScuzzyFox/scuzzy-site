b5ac4a6dbfe8350ee95a6c5821d46b3ce90bc39b
scuzzyfox <scuzzyfox@gmail.com>
2023-03-12 00:10:44 -0700

    deleted small unnecessary code

--- src/routes/tos/+page.server.ts
+++ src/routes/tos/+page.server.ts
@@ -1,8 +1,6 @@
 import type { PageServerLoad } from './$types';
-import { readFile } from 'fs/promises';
 import showdown from 'showdown';
 
-//should probably use the import file from "$lib/markdown/siteTos.md?raw" pattern here!!!!!!!!
 import siteTosRaw from '$lib/markdown/siteTos.md?raw';
 import commissionTosRaw from '$lib/markdown/commissionTos.md?raw';
 import FAQRaw from '$lib/markdown/FAQ.md?raw';
@@ -23,8 +21,3 @@ export const load = (async ({ params }) => {
 		willNotDrawHtml
 	};
 }) satisfies PageServerLoad;
-
-//reads data from file
-async function content(path: any) {
-	return await readFile(path, 'utf8');
-}
